c7954e7af37ef687442070d8a1b27fe0ffb792df
DanielaSalcedoMejia <danielasalcedo940@gmail.com>
2024-05-27 19:30:12 -0500

    configuracion

--- src/routes/usuario.route.ts
+++ src/routes/usuario.route.ts
@@ -29,9 +29,9 @@ router.post(
   ],
   crearUsuario
 );
-router.get("/", validateJWT, getUsuarios);
-router.get("/:id", validateJWT, getUnUsuario);
-router.put("/:id", validateJWT, updateUsuario);
-router.delete("/:id", validateJWT, eliminarUsuario);
+router.get("/", getUsuarios);
+router.get("/:id",getUnUsuario);
+router.put("/:id", updateUsuario);
+router.delete("/:id", eliminarUsuario);
 
 export default router;
